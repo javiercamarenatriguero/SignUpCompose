a4eec5516a84880f41a0808dfa72d0d924de0f7a
jcamarena-sh <61594082+jcamarena-sh@users.noreply.github.com>
2021-11-28 18:05:30 +0100

    Refactor Phone Number TextField

---
+++ app/src/main/java/com/akole/signupcompose/ui/screens/form/PhoneTextField.kt
@@ -0,0 +1,60 @@
+package com.akole.signupcompose.ui.screens.form
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.akole.signupcompose.R
+import com.akole.signupcompose.countryToPhonePrefix
+import com.akole.signupcompose.ui.CustomOutlinedTextField
+
+@Composable
+fun PhoneTextField(
+    phoneNumber: String,
+    country: String,
+    onPhoneNumberChange: (String) -> Unit,
+    onPhonePrefixClick: () -> Unit
+) {
+    CustomOutlinedTextField(
+        text = phoneNumber,
+        onValueChange = onPhoneNumberChange,
+        label = stringResource(id = R.string.sign_up_phone_number_label_text),
+        keyboardType = KeyboardType.Phone,
+        leadingIcon = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Phone,
+                    contentDescription = "",
+                    modifier = Modifier.padding(start = 10.dp, end = 5.dp)
+                )
+                Row(
+                    modifier = Modifier
+                        .padding(start = 5.dp, end = 5.dp)
+                        .clickable { onPhonePrefixClick() }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowDropDown,
+                        contentDescription = ""
+                    )
+                    Text(
+                        text = countryToPhonePrefix[country] ?: "Spain",
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+        }
+    )
+}
--- app/src/main/java/com/akole/signupcompose/ui/screens/form/SignUpContent.kt
+++ app/src/main/java/com/akole/signupcompose/ui/screens/form/SignUpContent.kt
@@ -1,6 +1,5 @@
 package com.akole.signupcompose.ui.screens.form
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,19 +8,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material.icons.filled.Phone
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.akole.signupcompose.R
-import com.akole.signupcompose.countryToPhonePrefix
 import com.akole.signupcompose.ui.CustomOutlinedTextField
 
 @Composable
@@ -63,36 +55,14 @@ fun SignUpContent(
                     )
                 }
             }
-            CustomOutlinedTextField(
-                text = viewModel.viewState.phoneNumber,
-                onValueChange = {
-                    viewModel.on(SignUpViewModel.ViewEvent.PhoneNumberChange(it))
+            PhoneTextField(
+                phoneNumber = viewModel.viewState.phoneNumber,
+                country = viewModel.viewState.country,
+                onPhoneNumberChange = { phoneNumber ->
+                    viewModel.on(SignUpViewModel.ViewEvent.PhoneNumberChange(phoneNumber))
                 },
-                label = stringResource(id = R.string.sign_up_phone_number_label_text),
-                leadingIcon = {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Phone,
-                            contentDescription = "",
-                            modifier = Modifier.padding(start = 10.dp, end = 5.dp)
-                        )
-                        Row(
-                            modifier = Modifier
-                                .padding(start = 5.dp, end = 5.dp)
-                                .clickable { viewModel.on(SignUpViewModel.ViewEvent.CountryClick) }
-                        ) {
-                            Icon(
-                                imageVector = Icons.Default.ArrowDropDown,
-                                contentDescription = ""
-                            )
-                            Text(
-                                text = countryToPhonePrefix[viewModel.viewState.country] ?: "Spain",
-                                fontWeight = FontWeight.Bold
-                            )
-                        }
-                    }
+                onPhonePrefixClick = {
+                    viewModel.on(SignUpViewModel.ViewEvent.CountryClick)
                 }
             )
         }
