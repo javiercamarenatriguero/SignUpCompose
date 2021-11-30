b5ad23c196af6c7244f1e42a30bcdaf6ae56c5a9
jcamarena-sh <61594082+jcamarena-sh@users.noreply.github.com>
2021-11-30 09:24:03 +0100

    Remove Preview

--- app/src/main/java/com/akole/signupcompose/ui/screens/signup/CountryModalSheetContent.kt
+++ app/src/main/java/com/akole/signupcompose/ui/screens/signup/CountryModalSheetContent.kt
@@ -1,6 +1,5 @@
 package com.akole.signupcompose.ui.screens.signup
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.defaultMinSize
@@ -8,17 +7,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.akole.signupcompose.R
 import com.akole.signupcompose.utils.countryToPhonePrefix
@@ -59,10 +54,4 @@ fun CountryListModalSheetContent(
             }
         }
     }
-}
-
-@Preview (showBackground = true)
-@Composable
-private fun previewModalSheet() {
-    CountryListModalSheetContent({})
 }
